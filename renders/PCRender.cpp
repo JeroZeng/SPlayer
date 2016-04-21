003aeef63941872122cf09b5e8a104cf00544c93
JeroZeng <jerozeng@163.com>
2016-04-21 21:41:34 +0800

    The 1st version of playDemo use glsl.

--- renders/PCRender.cpp
+++ renders/PCRender.cpp
@@ -1,81 +1,13 @@
+#include "GLFW/glad/glad.h"
 #include "PCRender.h"
 
-//set '1' to choose a type of file to play
-#define LOAD_RGB24   0
-#define LOAD_BGR24   0
-#define LOAD_BGRA    0
-#define LOAD_YUV420P 1
-
-#if LOAD_BGRA
-const int bpp=32;
-#elif LOAD_RGB24|LOAD_BGR24
-const int bpp=24;
-#elif LOAD_YUV420P
-const int bpp=12;
-#endif
-
-inline unsigned char CONVERT_ADJUST(double tmp)
+static const float vertices[] =
 {
-    return (unsigned char)((tmp >= 0 && tmp <= 255)?tmp:(tmp < 0 ? 0 : 255));
-}
-
-//YUV420P to RGB24
-void CONVERT_YUV420PtoRGB24(unsigned char* yuv_src,unsigned char* rgb_dst,int nWidth,int nHeight)
-{
-    unsigned char *tmpbuf=(unsigned char *)malloc(nWidth*nHeight*3);
-    unsigned char Y,U,V,R,G,B;
-    unsigned char* y_planar,*u_planar,*v_planar;
-    int rgb_width , u_width;
-    rgb_width = nWidth * 3;
-    u_width = (nWidth >> 1);
-    int ypSize = nWidth * nHeight;
-    int upSize = (ypSize>>2);
-    int offSet = 0;
-    
-    y_planar = yuv_src;
-    u_planar = yuv_src + ypSize;
-    v_planar = u_planar + upSize;
-    
-    for(int i = 0; i < nHeight; i++)
-    {
-        for(int j = 0; j < nWidth; j ++)
-        {
-            // Get the Y value from the y planar
-            Y = *(y_planar + nWidth * i + j);
-            // Get the V value from the u planar
-            offSet = (i>>1) * (u_width) + (j>>1);
-            V = *(u_planar + offSet);
-            // Get the U value from the v planar
-            U = *(v_planar + offSet);
-            
-            // Cacular the R,G,B values
-            // Method 1
-            R = CONVERT_ADJUST((Y + (1.4075 * (V - 128))));
-            G = CONVERT_ADJUST((Y - (0.3455 * (U - 128) - 0.7169 * (V - 128))));
-            B = CONVERT_ADJUST((Y + (1.7790 * (U - 128))));
-            /*
-             // The following formulas are from MicroSoft' MSDN
-             int C,D,E;
-             // Method 2
-             C = Y - 16;
-             D = U - 128;
-             E = V - 128;
-             R = CONVERT_ADJUST(( 298 * C + 409 * E + 128) >> 8);
-             G = CONVERT_ADJUST(( 298 * C - 100 * D - 208 * E + 128) >> 8);
-             B = CONVERT_ADJUST(( 298 * C + 516 * D + 128) >> 8);
-             R = ((R - 128) * .6 + 128 )>255?255:(R - 128) * .6 + 128;
-             G = ((G - 128) * .6 + 128 )>255?255:(G - 128) * .6 + 128; 
-             B = ((B - 128) * .6 + 128 )>255?255:(B - 128) * .6 + 128; 
-             */
-            offSet = rgb_width * i + j * 3;
-            
-            rgb_dst[offSet] = B;
-            rgb_dst[offSet + 1] = G;
-            rgb_dst[offSet + 2] = R;
-        }
-    }
-    free(tmpbuf);
-}
+    -1.0f, -1.0f, 0.0f, 1.0f,
+    1.0f, -1.0f, 1.0f, 1.0f,
+    -1.0f,  1.0f, 0.0f, 0.0f,
+    1.0f,  1.0f, 1.0f, 0.0f
+};
 
 PCRender::PCRender(SPlayer *player) {
     if (player != NULL)
@@ -83,31 +15,177 @@ PCRender::PCRender(SPlayer *player) {
 }
 
 PCRender::~PCRender() {
-    if (m_chConvertBuffer != NULL) {
-        free(m_chConvertBuffer);
-        m_chConvertBuffer = NULL;
-    }
 }
 
+
+GLuint PCRender::MakeShader(GLenum type, const char* text)
+{
+    GLuint shader;
+    GLint shader_ok;
+    GLsizei log_length;
+    char info_log[8192];
+
+    shader = glCreateShader(type);
+    if (shader != 0)
+    {
+        glShaderSource(shader, 1, (const GLchar**)&text, NULL);
+        glCompileShader(shader);
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &shader_ok);
+        if (shader_ok != GL_TRUE)
+        {
+            fprintf(stderr, "ERROR: Failed to compile %s shader\n", (type == GL_FRAGMENT_SHADER) ? "fragment" : "vertex");
+            glGetShaderInfoLog(shader, 8192, &log_length, info_log);
+            fprintf(stderr, "ERROR: \n%s\n\n", info_log);
+            glDeleteShader(shader);
+            shader = 0;
+        }
+    }
+    return shader;
+}
+
+GLuint PCRender::MakeShaderProgram(const char* vs_text, const char* fs_text)
+{
+    GLuint program = 0u;
+    GLint program_ok;
+    GLuint vertex_shader = 0u;
+    GLuint fragment_shader = 0u;
+    GLsizei log_length;
+    char info_log[8192];
+
+    vertex_shader = MakeShader(GL_VERTEX_SHADER, vs_text);
+    if (vertex_shader != 0u)
+    {
+        fragment_shader = MakeShader(GL_FRAGMENT_SHADER, fs_text);
+        if (fragment_shader != 0u)
+        {
+            /* make the program that connect the two shader and link it */
+            program = glCreateProgram();
+            if (program != 0u)
+            {
+                /* attach both shader and link */
+                glAttachShader(program, vertex_shader);
+                glAttachShader(program, fragment_shader);
+                glLinkProgram(program);
+                glGetProgramiv(program, GL_LINK_STATUS, &program_ok);
+
+                if (program_ok != GL_TRUE)
+                {
+                    fprintf(stderr, "ERROR, failed to link shader program\n");
+                    glGetProgramInfoLog(program, 8192, &log_length, info_log);
+                    fprintf(stderr, "ERROR: \n%s\n\n", info_log);
+                    glDeleteProgram(program);
+                    glDeleteShader(fragment_shader);
+                    glDeleteShader(vertex_shader);
+                    program = 0u;
+                }
+            }
+        }
+        else
+        {
+            fprintf(stderr, "ERROR: Unable to load fragment shader\n");
+            glDeleteShader(vertex_shader);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "ERROR: Unable to load vertex shader\n");
+    }
+    return program;
+}
 int PCRender::Init(SWindow *win) {
-    m_sPainter = new Painter(m_iWidth, m_iHeight);
-    m_sPainter->initGL(win);
     m_sWindow = win;
-    m_chConvertBuffer = (unsigned char*)malloc(m_iWidth * m_iHeight * sizeof(unsigned char)*3);
+    GLuint vertex_buffer;
+    GLint vpos_loc, tbuf_loc;
+
+    glfwMakeContextCurrent(m_sWindow);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    m_shaderProgram = MakeShaderProgram(vs, fs);
+    if (m_shaderProgram == 0u) {
+        return -1;
+    }
+
+    vpos_loc = glGetAttribLocation(m_shaderProgram , "vertexIn");
+    glEnableVertexAttribArray(vpos_loc);
+    glVertexAttribPointer(vpos_loc, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(float) * 4, (void*) 0);
+
+    tbuf_loc = glGetAttribLocation(m_shaderProgram , "textureIn");
+    glEnableVertexAttribArray(tbuf_loc);
+    glVertexAttribPointer(tbuf_loc, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(float) * 4, (void*) (sizeof(float) * 2));
+
+    //Init Texture
+    glGenTextures(1, &m_textureY);
+    glBindTexture(GL_TEXTURE_2D, m_textureY);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glGenTextures(1, &m_textureU);
+    glBindTexture(GL_TEXTURE_2D, m_textureU);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glGenTextures(1, &m_textureV);
+    glBindTexture(GL_TEXTURE_2D, m_textureV);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glfwMakeContextCurrent(NULL);
+
     return 0;
 }
 
 int PCRender::Draw(SBucket *bucket) {
-    m_sPainter->draw((unsigned char*)bucket->data);
-    /*
     glfwMakeContextCurrent(m_sWindow);
-    glRasterPos3f(-1.0f,1.0f,0);
-    glPixelZoom((float)m_iWidth/(float)m_iWidth, -(float)m_iHeight/(float)m_iHeight);
-    CONVERT_YUV420PtoRGB24((unsigned char*)bucket->data,m_chConvertBuffer,m_iWidth,m_iHeight);
-    glDrawPixels(m_iWidth, m_iHeight,GL_RGB, GL_UNSIGNED_BYTE, m_chConvertBuffer);
+    glUseProgram(m_shaderProgram);
+
+    int w, h;
+    glfwGetFramebufferSize(m_sWindow, &w, &h);
+    double rate = (double)w/m_iWidth>(double)h/m_iHeight?(double)h/m_iHeight:(double)w/m_iWidth;
+    glViewport((w-m_iWidth*rate)/2, (h-m_iHeight*rate)/2, m_iWidth*rate, m_iHeight*rate);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_textureY);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, m_iWidth, m_iHeight, 0, GL_RED, GL_UNSIGNED_BYTE, bucket->data);
+    GLuint textureUniformY = glGetUniformLocation(m_shaderProgram, "tex_y");
+    glUniform1i(textureUniformY, 0);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, m_textureU);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED,
+                 m_iWidth / 2, m_iHeight / 2, 0, GL_RED,
+                 GL_UNSIGNED_BYTE, bucket->data + m_iWidth * m_iHeight);
+    GLuint textureUniformU = glGetUniformLocation(m_shaderProgram, "tex_u");
+    glUniform1i(textureUniformU, 1);
+
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, m_textureV);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED,
+                 m_iWidth / 2,
+                 m_iHeight / 2, 0, GL_RED,
+                 GL_UNSIGNED_BYTE,
+                 bucket->data + (m_iWidth * m_iHeight * 5 / 4));
+    GLuint textureUniformV = glGetUniformLocation(m_shaderProgram, "tex_v");
+    glUniform1i(textureUniformV, 2);
+
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
     glfwSwapBuffers(m_sWindow);
     glfwMakeContextCurrent(NULL);
-    */
+
     return 0;
 }
 
--- renders/PCRender.h
+++ renders/PCRender.h
@@ -2,13 +2,42 @@
 #define _PC_RENDER_
 #include "../SPlayer.h"
 #include "../base/render.h"
-#include "Painter.h"
+
+static const char *vs =
+    "attribute vec2 vertexIn;"
+    "attribute vec2 textureIn;"
+    "varying vec2 textureOut;"
+    "void main()"
+    "{"
+    "     gl_Position = vec4(vertexIn, 0.0, 1.0);;"
+    "     textureOut = textureIn;"
+    "}";
+
+static const char *fs =
+    "varying vec2 textureOut;"
+    "uniform sampler2D tex_y;"
+    "uniform sampler2D tex_u;"
+    "uniform sampler2D tex_v;"
+    "void main(void)"
+    "{"
+    "    vec3 yuv;"
+    "    vec3 rgb;"
+    "    yuv.x = texture2D(tex_y, textureOut).x;"
+    "    yuv.y = texture2D(tex_u, textureOut).r - 0.5;"
+    "    yuv.z = texture2D(tex_v, textureOut).r - 0.5;"
+    "    rgb.r = yuv.x + (1.403 * yuv.z);"
+    "    rgb.g = yuv.x - (0.344 * yuv.y) - (0.714 * yuv.z);"
+    "    rgb.b = yuv.x + (1.770 * yuv.y);"
+    "     gl_FragColor = vec4(rgb, 1);"
+    "}";
 
 class PCRender : public Render {
 public:
     PCRender(SPlayer* player);
     ~PCRender();
     int Init(SWindow *win);
+    GLuint MakeShader(GLenum type, const char *ch);
+    GLuint MakeShaderProgram(const char *vs, const char *fs);
     int Draw(SBucket *bucket);
     int ClearScreen();
     int WaitThreadClose();
@@ -17,11 +46,13 @@ public:
 
 public:
     SPlayer *m_sPlayer;
-    Painter *m_sPainter;
-    unsigned char *m_chConvertBuffer;
 
 private:
     SWindow *m_sWindow; 
+    GLuint m_shaderProgram;
+    GLuint m_textureY;
+    GLuint m_textureU;
+    GLuint m_textureV;
 };
 
 #endif//_PC_RENDER_
