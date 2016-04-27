adc88dc7376c47b58509fdc89207375c6746f1d8
JeroZeng <jerozeng@163.com>
2016-04-27 14:09:12 +0800

    Add function ReadFrame for H264Demuxer.

--- demuxers/H264Demuxer.cpp
+++ demuxers/H264Demuxer.cpp
@@ -1,15 +1,64 @@
 #include "H264Demuxer.h"
 
+H264Demuxer::H264Demuxer() {
+    m_bReadFile = true;
+    m_iUsedSize = 0;
+    m_iNextFrameSize = 0;
+}
+
 int H264Demuxer::Open(const char *url) {
     m_pFile = fopen(url, "r");
     if (m_pFile == (FILE*)NULL) {
         printf("Cant open input 264 file\n");
         return 1;
     }
-    memBuf = (uint8_t*)malloc(BUFFER_2M * sizeof(uint8_t));
+    m_MemBuf = (uint8_t*)malloc(BUFFER_SIZE * sizeof(uint8_t));
     return 0;
 }
 
-int GetOneFrame(SBucket *bucket) {
-    
+int H264Demuxer::GetOneFrame(SBucket *bucket) {
+    int i = 0;
+    if (m_bReadFile) {
+        fread(m_MemBuf, BUFFER_SIZE, 1, m_pFile);
+        m_bReadFile = false;
+    }
+    if (m_iNextFrameSize == 0) {
+        for( i=0; i<BUFFER_SIZE-m_iUsedSize; i++) {
+            if ((m_MemBuf[m_iUsedSize+i] == 0 &&
+                 m_MemBuf[m_iUsedSize+i+1] == 0 &&
+                 m_MemBuf[m_iUsedSize+i+2] == 0 &&
+                 m_MemBuf[m_iUsedSize+i+3] == 1) ||
+                (m_MemBuf[m_iUsedSize+i] == 0 &&
+                 m_MemBuf[m_iUsedSize+i+1] == 0 &&
+                 m_MemBuf[m_iUsedSize+i+2] == 1)) {
+                if (i<4)
+                    continue;
+                break;
+            }
+        }
+        m_iNextFrameSize = i;
+    }
+    bucket->size = m_iNextFrameSize;
+    m_iUsedSize++;
+    memcpy(bucket->data, m_MemBuf, m_iNextFrameSize);
+    m_iUsedSize += m_iNextFrameSize;    
+    for( i=0; i<BUFFER_SIZE-m_iUsedSize; i++) {
+        if ((m_MemBuf[m_iUsedSize+i] == 0 &&
+             m_MemBuf[m_iUsedSize+i+1] == 0 &&
+             m_MemBuf[m_iUsedSize+i+2] == 0 &&
+             m_MemBuf[m_iUsedSize+i+3] == 1) ||
+            (m_MemBuf[m_iUsedSize+i] == 0 &&
+             m_MemBuf[m_iUsedSize+i+1] == 0 &&
+             m_MemBuf[m_iUsedSize+i+2] == 1)) {
+            if (i<4)
+                continue;
+            break;
+        }
+    }
+    m_iNextFrameSize = i;
+    return i;
+}
+
+void H264Demuxer::Reset() {
+
 }
--- demuxers/H264Demuxer.h
+++ demuxers/H264Demuxer.h
@@ -3,19 +3,23 @@
 
 #include "../base/demuxer.h"
 
-#define BUFFER_2M (2 << 20)
+#define BUFFER_SIZE (2 << 20)
 
-class H264Demuer: public Demuxer {
+class H264Demuxer: public Demuxer {
 public:
 
 public:
+    H264Demuxer();
     int Open(const char *url);
     int GetOneFrame(SBucket *bucket);
     void Reset();
 
 private:
     FILE *m_pFile;
-    unsigned char *mem_buf;
+    unsigned char *m_MemBuf;
+    bool m_bReadFile;
+    uint32_t m_iUsedSize;
+    uint32_t m_iNextFrameSize;
 };
 
 #endif//_H264_DEMUXER_H_
